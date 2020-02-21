1268a7027be2f1a497f1a23a9231810480d845df
Meshal Al-Sultan <51976236+meshalalsultan@users.noreply.github.com>
2020-02-22 02:20:42 +0300

    Add files via upload

---
+++ def_name_age.py
@@ -0,0 +1,19 @@
+user = input ('type your name ')
+
+age = input ('type your age ')
+
+
+def my_func(user,age):
+  if user == "meshal" :
+    print ('yes mesho you did it in the name')
+  else:
+    print (' Sorry ,, you not do it in the name')
+
+  if age == '35' :
+    print ('yes meshoo ,, you did it in the age')
+  else:
+    print('Sorry , you not did it in the age ')
+
+  
+
+my_func(user,age)
